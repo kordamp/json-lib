8b6866fd4a16175359698c571fcc83839866182a
aalmiray <aalmiray>
2008-01-20 20:55:19 +0000

    [json-lib] added spring extension (finally)

---
+++ extensions/spring/src/main/java/net/sf/json/spring/web/servlet/view/JsonView.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2006-2008 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.sf.json.spring.web.servlet.view;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSON;
+import net.sf.json.JSONSerializer;
+import net.sf.json.JsonConfig;
+
+import org.springframework.web.servlet.view.AbstractView;
+
+/**
+ * A View that renders its model as a JSON object.
+ *
+ * @author Andres Almiray <aalmiray@users.sourceforge.net>
+ */
+public class JsonView extends AbstractView {
+   /** Default content type. Overridable as bean property. */
+   private static final String DEFAULT_JSON_CONTENT_TYPE = "application/json";
+   /** Json confiiguration */
+   private JsonConfig jsonConfig = new JsonConfig();
+   /** Set of properties to be excluded in the JSON rendering. */
+   private String[] excludedProperties;
+   /**
+    * Tells the JSONSerializer to ignore or not its internal property
+    * exclusions.
+    */
+   private boolean ignoreDefaultExcludes;
+
+   public JsonView() {
+      super();
+      setContentType( DEFAULT_JSON_CONTENT_TYPE );
+   }
+
+   public JsonConfig getJsonConfig(){
+      return jsonConfig;
+   }
+
+   public void setJsonConfig( JsonConfig jsonConfig ){
+     this.jsonConfig = jsonConfig != null ? jsonConfig : new JsonConfig();
+   }
+   
+   /**
+    * Returns wether the JSONSerializer will ignore or not its internal property
+    * exclusions.
+    */
+   public boolean isIgnoreDefaultExcludes() {
+      return jsonConfig.isIgnoreDefaultExcludes();
+   }
+
+   /**
+    * Sets the group of properties to be excluded.
+    */
+   public void setExcludedProperties( String[] excludedProperties ) {
+      jsonConfig.setExcludes( excludedProperties );
+   }
+
+   /**
+    * Sets wether the JSONSerializer will ignore or not its internal property
+    * exclusions.
+    */
+   public void setIgnoreDefaultExcludes( boolean ignoreDefaultExcludes ) {
+      jsonConfig.setIgnoreDefaultExcludes( ignoreDefaultExcludes );
+   }
+
+   /**
+    * Creates a JSON [JSONObject,JSONArray,JSONNUll] from the model values.
+    */
+   protected JSON createJSON( Map model, HttpServletRequest request, HttpServletResponse response ) {
+      return defaultCreateJSON( model );
+   }
+
+   /**
+    * Creates a JSON [JSONObject,JSONArray,JSONNUll] from the model values.
+    */
+   protected final JSON defaultCreateJSON( Map model ) {
+      return JSONSerializer.toJSON( model, jsonConfig );
+   }
+
+   /**
+    * Returns the group of properties to be excluded.
+    */
+   protected String[] getExcludedProperties() {
+      return excludedProperties;
+   }
+
+   protected void renderMergedOutputModel( Map model, HttpServletRequest request,
+         HttpServletResponse response ) throws Exception {
+      response.setContentType( getContentType() );
+      writeJSON( model, request, response );
+   }
+
+   protected void writeJSON( Map model, HttpServletRequest request,
+         HttpServletResponse response ) throws Exception {
+      
+      JSON json = createJSON( model, request, response );
+      json.write( response.getWriter() );
+   }
+}
---
+++ extensions/spring/src/test/java/net/sf/json/spring/web/servlet/view/JsonViewTest.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright 2006-2008 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.sf.json.spring.web.servlet.view;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import junit.framework.TestCase;
+import net.sf.jstester.JsTester;
+import net.sf.json.JSONSerializer;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.web.context.support.StaticWebApplicationContext;
+
+/**
+ * @author Andres Almiray
+ */
+public class JsonViewTest extends TestCase {
+   public static void main( String[] args ) {
+      junit.textui.TestRunner.run( JsonViewTest.class );
+   }
+
+   private JsTester jsTester;
+   private MockServletContext servletContext;
+   private MockHttpServletRequest servletRequest;
+   private MockHttpServletResponse servletResponse;
+
+   public JsonViewTest( String testName ) {
+      super( testName );
+   }
+
+   public void testRenderObjectGraph() throws Exception {
+      JsonView view = new JsonView();
+      Map model = new HashMap();
+      model.put( "bool", Boolean.TRUE );
+      model.put( "integer", new Integer( 1 ) );
+      model.put( "str", "string" );
+      Map bean = new HashMap();
+      bean.put( "name", "mybean" );
+      bean.put( "bools", new boolean[] { true, false } );
+      model.put( "bean", bean );
+
+      view.render( model, servletRequest, servletResponse );
+      jsTester.eval( toJsScript( servletResponse ) );
+
+      jsTester.assertNotNull( "json" );
+      jsTester.assertIsObject( "json" );
+      jsTester.assertEquals( "json.bool", "true" );
+      jsTester.assertEquals( "json.integer", "1" );
+      jsTester.assertEquals( "json.str", "'string'" );
+      jsTester.assertIsObject( "json.bean" );
+      jsTester.assertIsArray( "json.bean.bools" );
+      jsTester.assertEquals( "2", "json.bean.bools.length" );
+      jsTester.assertEquals( "'mybean'", "json.bean.name" );
+   }
+
+   public void testRenderObjectGraphWithPropertyExclusion() throws Exception {
+      JsonView view = new JsonView();
+      view.setExcludedProperties( new String[] { "bools" } );
+      Map model = new HashMap();
+      model.put( "bool", Boolean.TRUE );
+      model.put( "integer", new Integer( 1 ) );
+      model.put( "str", "string" );
+      Map bean = new HashMap();
+      bean.put( "name", "mybean" );
+      bean.put( "bools", new boolean[] { true, false } );
+      model.put( "bean", bean );
+
+      view.render( model, servletRequest, servletResponse );
+      jsTester.eval( toJsScript( servletResponse ) );
+
+      jsTester.assertNotNull( "json" );
+      jsTester.assertIsObject( "json" );
+      jsTester.assertEquals( "json.bool", "true" );
+      jsTester.assertEquals( "json.integer", "1" );
+      jsTester.assertEquals( "json.str", "'string'" );
+      jsTester.assertIsObject( "json.bean" );
+      jsTester.assertIsUndefined( "json.bean.bools" );
+      jsTester.assertEquals( "'mybean'", "json.bean.name" );
+   }
+
+   public void testRenderSimpleProperties() throws Exception {
+      JsonView view = new JsonView();
+      Map model = new HashMap();
+      model.put( "bool", Boolean.TRUE );
+      model.put( "integer", new Integer( 1 ) );
+      model.put( "str", "string" );
+
+      view.render( model, servletRequest, servletResponse );
+      jsTester.eval( toJsScript( servletResponse ) );
+
+      jsTester.assertNotNull( "json" );
+      jsTester.assertIsObject( "json" );
+      jsTester.assertEquals( "json.bool", "true" );
+      jsTester.assertEquals( "json.integer", "1" );
+      jsTester.assertEquals( "json.str", "'string'" );
+   }
+
+   protected void setUp() throws Exception {
+      servletContext = new MockServletContext();
+      StaticWebApplicationContext wac = new StaticWebApplicationContext();
+      wac.setServletContext( servletContext );
+      servletRequest = new MockHttpServletRequest( servletContext );
+      servletResponse = new MockHttpServletResponse();
+
+      jsTester = new JsTester();
+      jsTester.onSetUp();
+   }
+
+   protected void tearDown() throws Exception {
+      jsTester.onTearDown();
+   }
+
+   private String toJsScript( MockHttpServletResponse response ) throws Exception {
+      // looks like MockHttpServletResponse is broken
+      String json = response.getContentAsString();
+      if( json.startsWith("{") && !json.endsWith("}") ){ json += "}"; }
+      if( json.startsWith("[") && !json.endsWith("]") ){ json += "]"; }
+      return "var json = eval('(" + json + ")');";
+   }
+}
