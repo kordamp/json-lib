637a2d013bbcb3fafa30da4358babce7ec026692
aalmiray <aalmiray>
2008-06-10 04:46:46 +0000

    [json-lib-ext-spring] fix for 1969999

--- extensions/spring/src/main/java/net/sf/json/spring/web/servlet/view/JsonView.java
+++ extensions/spring/src/main/java/net/sf/json/spring/web/servlet/view/JsonView.java
@@ -25,6 +25,8 @@ import net.sf.json.JSON;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONSerializer;
 import net.sf.json.JsonConfig;
+import net.sf.json.util.PropertyFilter;
+import net.sf.json.filters.OrPropertyFilter;
 
 import org.springframework.web.servlet.view.AbstractView;
 
@@ -37,6 +39,7 @@ public class JsonView extends AbstractView {
    /** Default content type. Overridable as bean property. */
    private static final String DEFAULT_JSON_CONTENT_TYPE = "application/json";
    private boolean forceTopLevelArray = false;
+   private boolean skipBindingResult = true;
    /** Json confiiguration */
    private JsonConfig jsonConfig = new JsonConfig();
    
@@ -54,11 +57,19 @@ public class JsonView extends AbstractView {
    }
 
    /**
-    * Returns wether the JSONSerializer will ignore or not its internal property
+    * Returns whether the JSONSerializer will ignore or not its internal property
     * exclusions.
     */
    public boolean isIgnoreDefaultExcludes() {
       return jsonConfig.isIgnoreDefaultExcludes();
+   }
+
+   /**
+    * Returns whether the JSONSerializer will skip or not any BindingResult related keys on the model.<p>
+    * Models in Spring >= 2.5 will cause an exception as they contain a BindingResult that cycles back.
+    */
+   public boolean isSkipBindingResult() {
+      return skipBindingResult;
    }
 
    /**
@@ -73,15 +84,31 @@ public class JsonView extends AbstractView {
    }
 
    /**
-    * Sets wether the JSONSerializer will ignore or not its internal property
+    * Sets whether the JSONSerializer will ignore or not its internal property
     * exclusions.
     */
    public void setIgnoreDefaultExcludes( boolean ignoreDefaultExcludes ) {
       jsonConfig.setIgnoreDefaultExcludes( ignoreDefaultExcludes );
    }
 
-   public void setJsonConfig( JsonConfig jsonConfig ){
+   public void setJsonConfig( JsonConfig jsonConfig ) {
      this.jsonConfig = jsonConfig != null ? jsonConfig : new JsonConfig();
+     if( skipBindingResult ){
+        PropertyFilter jsonPropertyFilter = this.jsonConfig.getJsonPropertyFilter();
+        if( jsonPropertyFilter == null ) {
+           this.jsonConfig.setJsonPropertyFilter( new BindingResultPropertyFilter() );
+        }else{
+           this.jsonConfig.setJsonPropertyFilter( new OrPropertyFilter(new BindingResultPropertyFilter(), jsonPropertyFilter) );
+        }
+     }
+   }
+
+   /**
+    * Sets whether the JSONSerializer will skip or not any BindingResult related keys on the model.<p>
+    * Models in Spring >= 2.5 will cause an exception as they contain a BindingResult that cycles back.
+    */
+   public void setSkipBindingResult( boolean kipBindingResult ) {
+      this.skipBindingResult = skipBindingResult;
    }
 
    /**
@@ -95,6 +122,9 @@ public class JsonView extends AbstractView {
     * Creates a JSON [JSONObject,JSONArray,JSONNUll] from the model values.
     */
    protected final JSON defaultCreateJSON( Map model ) {
+      if( skipBindingResult && jsonConfig.getJsonPropertyFilter() == null ){
+         this.jsonConfig.setJsonPropertyFilter( new BindingResultPropertyFilter() );
+      }
       return JSONSerializer.toJSON( model, jsonConfig );
    }
 
@@ -118,5 +148,11 @@ public class JsonView extends AbstractView {
          json = new JSONArray().element(json);
       }
       json.write( response.getWriter() );
+   }
+
+   private static class BindingResultPropertyFilter implements PropertyFilter {
+      public boolean apply( Object source, String name, Object value ) {
+         return name.startsWith("org.springframework.validation.BindingResult.");
+      }
    }
 }
