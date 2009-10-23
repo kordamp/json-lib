99aa6bdc2f66098c0a5ee8cf53e21e69f2fb449d
Kohsuke Kawaguchi <kk@kohsuke.org>
2009-10-22 17:08:08 -0700

    prepared to ship a new version

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-VERSION="2.1-rev3"
+#!/bin/bash
+VERSION=$(show-pom-version pom.xml)
 GROUPID="org.kohsuke.stapler"
 ARTIFACTID="json-lib"
 REPO_URL="java-net:/maven2-repository/trunk/repository/"
